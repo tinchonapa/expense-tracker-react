fc3eb0e194a196ad02cf8062270c2d4228d86be3
Martin Naparstek <tinchonapa@gmail.com>
2021-07-10 10:10:36 -0600

    Created Transaction components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import './App.css';
 
@@ -12,6 +14,8 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
